bc12dd06375d11a48456b558922408573abb54aa
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-15 11:49:53 -0300

    module to search element in enumerable

--- lib/searcher.ex
+++ lib/searcher.ex
@@ -1,18 +1,49 @@
 defmodule Searcher do
   @moduledoc """
-  Documentation for `Searcher`.
+  Module to find element of an enumerable performatically.
   """
 
   @doc """
-  Hello world.
+  Find element of an enumerable.
+  Returns the any element that evaluation in predicate was true.
+
+  ## Parameters
+   - enumerable: an Enumerable
+   - function: predicate to evaluate item
 
   ## Examples
 
-      iex> Searcher.hello()
-      :world
-
+      iex> Searcher.find([1, 2, 3], fn item -> item === 3 end)
+      3
   """
-  def hello do
-    :world
+  def find(enumerable, function) when is_function(function) do
+    search(enumerable, function)
+  end
+  
+  defp search(enumerable, function) do
+    count = enumerable
+    |> Enum.map(&evaluate_item_async(&1, function))
+    |> Enum.count()
+    
+    handle_response(count)
+  end
+
+  defp evaluate_item_async(item, function) do
+    current = self()
+
+    spawn_link(fn -> 
+      case !!function.(item) do
+        true -> send(current, {item, true})
+        _ -> send(current, {item, false})
+      end
+    end)
+  end
+
+  defp handle_response(count) when count == 0, do: nil
+  defp handle_response(count) do
+    receive do
+      {item, true} -> item
+      _ -> handle_response(count - 1)
+    end
   end
 end
--- test/searcher_test.exs
+++ test/searcher_test.exs
@@ -1,8 +1,44 @@
 defmodule SearcherTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Searcher
 
-  test "greets the world" do
-    assert Searcher.hello() == :world
+  describe "find/2" do
+    test "when find element of list, returns the element" do
+      list = [1, 2, 3, 4, 5, 6, 7]
+
+      expected = 7
+
+      result = Searcher.find(list, fn x -> x === expected end)
+
+      assert result == expected
+    end
+
+    test "when find element of list and doesn't have my element, returns nil" do
+      list = [1, 2, 3, 4, 5, 6, 6, 7]
+
+      result = Searcher.find(list, fn x -> x > 5 end)
+
+      assert result > 5 
+    end
+
+    test "when find element of big range, returns the element" do
+      range = 1..100_000_000
+
+      expected = 90_000_000
+
+      result = Searcher.find(range, fn x -> x === expected end)
+
+      assert result == expected
+    end
+
+    test "when find element of empty list, returns nil" do
+      list = []
+
+      expected = nil
+
+      result = Searcher.find(list, fn x -> x end)
+
+      assert result == expected
+    end
   end
 end
